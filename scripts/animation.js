c5b4095712a250275f871d18e66c72eefde4b3ce
Alex Kang <akang95@gmail.com>
2018-03-07 19:10:46 -0800

    Added credits and lowered exhibit item height

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	// Constants.
 	const PIECES = [
-		'<iframe src="https://player.vimeo.com/video/217353881?byline=0&portrait=0" width="889" '
-			+ 'height="500" frameborder="0" webkitallowfullscreen mozallowfullscreen '
+		'<iframe src="https://player.vimeo.com/video/217353881?byline=0&portrait=0" width="711" '
+			+ 'height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen '
 			+ 'allowfullscreen></iframe>',
 		"flamingo.gif"
 	];	
