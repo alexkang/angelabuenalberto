1d22325b25ac48203fe8988ebc5349dc530ae802
Alex Kang <akang95@gmail.com>
2018-07-12 23:26:58 -0700

    Updated portfolio

--- scripts/design.js
+++ scripts/design.js
@@ -5,19 +5,21 @@ $(document).ready(function(){
 		"2.png",
 		"3.png",
 		"4.png",
-		"5.jpg",
+		"5.png",
 		"6.png",
 		"7.png",
 		"8.png",
 		"9.png",
-		"10.jpg",
+		"10.png",
 		"11.png",
 		"12.png",
-		"13.jpg",
-		"14.png",
+		"13.png",
+		"14.jpg",
 		"15.png",
-		"16.png",
-		"17.png"
+		"16.gif",
+		"17.jpg",
+		"18.png",
+		
 	];	
 
 	// Start of script.
