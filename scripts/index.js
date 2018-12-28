4cbfa16505001ef30d50ba51041c07ad4937c8aa
Alex Kang <alexanderkang@alexanderkang-macbookpro.roam.corp.google.com>
2018-12-27 21:20:46 -0800

    Aesthetic adjustments

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
   // ---- Constants ----
 
-  var EXHIBIT_ITEM_PADDING_PERCENTAGE = 1.6;
+  var EXHIBIT_ITEM_PADDING_PERCENTAGE = 1.2;
   var VIMEO_EMBED_PREFIX = "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://player.vimeo.com/video/"
   var VIMEO_EMBED_SUFFIX = "' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>"
   var ANIMATION_VIMEO_VIDEO_IDS = ["273060713", "283148908", "227518868", "217353881"];
@@ -36,7 +36,7 @@ $(document).ready(() => {
   }
 
   function calculateExhibitItemWidthPercentage(itemsPerRow) {
-    return (100 - (2 * EXHIBIT_ITEM_PADDING_PERCENTAGE * itemsPerRow)) / itemsPerRow;
+    return (100 - (EXHIBIT_ITEM_PADDING_PERCENTAGE * itemsPerRow)) / itemsPerRow;
   }
 
   function loadExhibit(containingElement, folder, size, itemsPerRow = 3) {
